de88d26f497406d3ac0aeb1a52320811294b4bd7
NicolaFerracin <nicola.ferracin@gmail.com>
2017-06-01 23:42:27 +0200

    Finish module 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController.js')
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get('/', storeController.myMiddleware, storeController.homePage);
+
+router.get('/example', (req, res) => {
+  const obj = { name: 'Obj', type: 'object' };
+  // res.send('Hey! It works!');
+  // res.json(obj);
+  res.render('hello', obj);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  res.send([...req.params.name].reverse().join(''));
 });
 
 module.exports = router;
