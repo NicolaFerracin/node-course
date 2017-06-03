de1600a7a18a26dedaeae2129e31c43f41e760a4
NicolaFerracin <nicola.ferracin@gmail.com>
2017-06-03 16:25:40 +0200

    Finish module 7

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -65,10 +65,19 @@ exports.updateStore = async (req, res) => {
     res.redirect(`/stores/${store._id}/edit`);
 }
 
-exports.getStore = async (req, res, next) => {
+exports.getStoreBySlug = async (req, res, next) => {
     const store = await Store.findOne({ slug: req.params.slug });
     if (!store) {
         return next();
     }
     res.render('store', { title: store.name, store });
+}
+
+exports.getStoresByTag = async (req, res) => {
+    const tag = req.params.tag;
+    const tagQuery = tag || { $exists: true };
+    const tagsPromise = Store.getTagsList();
+    const storesPromise = Store.find({ tags: tagQuery });
+    const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+    res.render('tag', { title: 'Tags', tags, tag, stores });
 }
