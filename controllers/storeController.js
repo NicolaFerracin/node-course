eae2d190f1e4047306d3477ca2e01537df125b29
NicolaFerracin <nicola.ferracin@gmail.com>
2017-06-06 19:57:27 +0200

    Finish module 14

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -48,8 +48,24 @@ exports.createStore = async (req, res) => {
 }
 
 exports.getStores = async (req, res) => {
-    const stores = await Store.find();
-    res.render('stores', { title: 'Stores', stores })
+    const page = req.params.page || 1;
+    const limit = 4;
+    const skip = (page * limit) - limit;
+    const storesPromise = Store
+        .find()
+        .skip(skip)
+        .limit(limit);
+    const countPromise = Store.count();
+    const [stores, count] = await Promise.all([storesPromise, countPromise]);
+    const pages = Math.ceil(count / limit);
+
+    if (!stores.length && skip) {
+        req.flash('info', `Hey, you are lookiong for page ${page} but it doesn't seem to exist so you are now on page ${pages}`);
+        res.redirect(`/stores/page/${pages}`);
+        return;
+    }
+
+    res.render('stores', { title: 'Stores', stores, page, pages, count })
 }
 
 const confirmOwner = (store, user) => {
