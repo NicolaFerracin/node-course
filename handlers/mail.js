f0ef17c34711f5859cbc8468642b5a4478c45dc3
NicolaFerracin <nicola.ferracin@gmail.com>
2017-06-06 22:23:05 +0200

    Finish module 15

--- handlers/mail.js
+++ handlers/mail.js
@@ -1,34 +1,30 @@
-const nodemailer = require('nodemailer');
 const pug = require('pug');
 const juice = require('juice');
 const htmlToText = require('html-to-text');
-const promisify = require('es6-promisify');
-
-const transport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    }
-});
+const sg = require('sendgrid')(process.env.MAIL_API_KEY);
+const helper = require('sendgrid').mail;
 
 const generateHtml = (filename, options = {}) => {
     const html = pug.renderFile(`${__dirname}/../views/email/${filename}.pug`, options);
     const inlined = juice(html);
-    return inlined;    
+    return inlined;
 }
 
 exports.send = async (options) => {
     const html = generateHtml(options.filename, options);
-    const text = htmlToText.fromString(html);
-    const mailOptions = {
-        from: 'Nicola Ferracin <nicola.ferracin@gmail.com>',
-        to: options.user.email,
-        subject: options.subject,
-        html,
-        text
-    };
-    const sendMail = promisify(transport.sendMail, transport);
-    return sendMail(mailOptions);
+    const fromEmail = { email: 'nicola.ferracin@gmail.com', name: 'Nicola Ferracin'};
+    const toEmail = { email: options.user.email };
+    const subject = options.subject;
+    const content = new helper.Content('text/html', html);
+    const mail = new helper.Mail(fromEmail, subject, toEmail, content);
+    
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON()
+    });
+
+    const sendMail = sg.API(request);
+    console.log(sendMail)
+    return sendMail;
 }
